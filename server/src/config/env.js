29a1bc95b3f428b2c8f0c887c2d7990f9b0241ab
Dmitry <shattlexoff@gmail.com>
2026-04-15 22:42:17 +0200

    Prepare Render deployment (blueprint + PORT support)

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,5 +1,5 @@
 export const env = {
-  port: Number(process.env.API_PORT ?? 8787),
+  port: Number(process.env.PORT ?? process.env.API_PORT ?? 8787),
   corsOrigin: process.env.CORS_ORIGIN ?? "http://127.0.0.1:5173",
   bitrixWebhookUrl: process.env.BITRIX24_WEBHOOK_URL ?? "",
   bitrixLeadSource: process.env.BITRIX24_LEAD_SOURCE ?? "WEBSITE",
